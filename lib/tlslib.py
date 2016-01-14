afd314f134eaf74c6ed7bef17c45e24e860dd19e
James Beedy <jamesbeedy@gmail.com>
2016-01-14 12:50:27 -0800

    Working on tests

--- lib/tlslib.py
+++ lib/tlslib.py
@@ -5,82 +5,118 @@ from charmhelpers.core import hookenv
 from charmhelpers.core import unitdata
 
 
-def server_cert(destination_directory):
+def server_cert(destination_directory, key_path=None):
     """
     Copy server cert to destination_directory
 
     @param string destination_directory dest dir for server cert
     """
+
+    # Must remove the path characters from the local unit name.
+    path_name = hookenv.local_unit().replace('/', '_')
+
+    # Optional key_path for unit tests
+    if not key_path:
+        local_key_path = 'easy-rsa/easyrsa3/pki/private/{0}.key'.format(
+                         path_name)
+    else:
+        local_key_path = key_path
+
     #Save the server certificate from unitdata to dest_dir
     save_certificate(destination_directory, 'server')
     # Copy the unitname.key to dest_dir/server.key
-    copy_key(destination_directory, 'server')
+    copy_key(destination_directory, 'server', local_key_path)
 
 
-def client_cert(destination_directory):
+def client_cert(destination_directory, cert_path=None, key_path=None):
     """
     Copy client cert to destination_directory
 
     @param string destination_directory dest dir for client cert
     """
+
+    # Optional cert path for unit tests 
+    if not cert_path:
+        client_cert_path = 'easy-rsa/easyrsa3/pki/issued/client.crt'
+    else:
+        client_cert_path = cert_path
+
+    # Optional key path for unit tests 
+    if not key_path:
+        client_key_path = 'easy-rsa/easyrsa3/pki/private/client.key'
+    else:
+        client_key_path = key_path
+
     # Check for directory existence
     if not os.path.isdir(destination_directory):
         os.makedirs(destination_directory)
         os.chmod(destination_directory, 0o770)
+
     # The client certificate is also available on charm unitdata.
-    client_cert_path = 'easy-rsa/easyrsa3/pki/issued/client.crt'
     webapp_cert_path = os.path.join(destination_directory, 'client.crt')
+
+    # Check for client cert
     if os.path.isfile(client_cert_path):
         # Copy the client.crt to dest_dir/client.crt
         copy2(client_cert_path, webapp_cert_path)
+
     # The client key is only available on the leader.
-    client_key_path = 'easy-rsa/easyrsa3/pki/private/client.key'
     webapp_key_path = os.path.join(destination_directory, 'client.key')
+
     if os.path.isfile(client_key_path):
         # Copy the client.key to dest_dir/client.key
         copy2(client_key_path, webapp_key_path)
 
 
-def ca(directory):
+def ca(directory, cert_path=None):
     """
     When the Certificate Authority is available, copy the CA from the
     /usr/local/share/ca-certificates/<service_name>.crt to the proper directory.
 
     @param string directory dest dir for crt
+    @param string cert_path source cert path
     """
+
+    # Normally the CA is just on the leader, but the tls layer installs the
+    # CA on all systems in the /usr/local/share/ca-certificates directory.
+    # Optional cert_path for unit test
+    if not cert_path:
+        ca_path = '/usr/local/share/ca-certificates/{0}.crt'.format(
+                  hookenv.service_name())
+    else:
+        ca_path = cert_path
+
     # Ensure the dest_dir exists.
     if not os.path.isdir(directory):
         os.makedirs(directory)
         os.chmod(directory, 0o770)
-    # Normally the CA is just on the leader, but the tls layer installs the
-    # CA on all systems in the /usr/local/share/ca-certificates directory.
-    ca_path = '/usr/local/share/ca-certificates/{0}.crt'.format(
-              hookenv.service_name())
+
     # The CA should be copied to the destination directory and named 'ca.crt'.
     destination_ca_path = os.path.join(directory, 'ca.crt')
     if os.path.isfile(ca_path):
         copy2(ca_path, destination_ca_path)
 
-def copy_key(directory, prefix):
+def copy_key(directory, prefix, key_path):
     """
     Copy the key from the easy-rsa/easyrsa3/pki/private directory to the
     specified directory.
     
     @param string directory dest dir for key
-    @param string prefix prefix for cert
+    @param string prefix prefix for key
+    @param string key_path source key path
     """
+
+    # Ensure the dest_dir exists.
     if not os.path.isdir(directory):
         os.makedirs(directory)
         os.chmod(directory, 0o770)
-    # Must remove the path characters from the local unit name.
-    path_name = hookenv.local_unit().replace('/', '_')
+
     # The key is not in unitdata it is in the local easy-rsa directory.
-    local_key_path = 'easy-rsa/easyrsa3/pki/private/{0}.key'.format(path_name)
     key_name = '{0}.key'.format(prefix)
     # The key should be copied to this directory.
     destination_key_path = os.path.join(directory, key_name)
     # Copy the key file from the local directory to the destination.
-    copy2(local_key_path, destination_key_path)
+    copy2(key_path, destination_key_path)
 
 
 def save_certificate(directory, prefix):
@@ -103,4 +139,4 @@ def save_certificate(directory, prefix):
     certificate_path = os.path.join(directory, certificate_name)
     # write the server certificate out to the correct location
     with open(certificate_path, 'w') as fp:
-        fp.write(certificate_data)
+        fp.write(str(certificate_data))
--- unit_tests/test_actions.py
+++ unit_tests/test_actions.py
@@ -1,21 +1,35 @@
-#!/usr/bin/env python
-
+#!/usr/bin/python3
+import pytest
 import sys
-import mock
-import unittest
 from pkg_resources import resource_filename
 
 # allow importing actions from the lib directory
 sys.path.append(resource_filename(__name__, '../lib'))
-import actions
 
 
-class TestActions(unittest.TestCase):
-    @mock.patch('charmhelpers.core.hookenv.log')
-    def test_log_start(self, log):
-        actions.log_start('test-service')
-        log.assert_called_once_with('layer-tls starting')
+@pytest.fixture
+def ca():
+    from tlslib import ca as cert_auth
+    cert_auth("/tmp", 'tls-test.crt')
 
+def test_ca(monkeypatch):
+    monkeypatch.setenv('JUJU_UNIT_NAME', 'test-tls')
+    ca()
 
-if __name__ == '__main__':
-    unittest.main()
+@pytest.fixture
+def client_cert():
+    from tlslib import client_cert as cc
+    cc("/tmp", 'tls-test.crt', 'tls-test.key')
+
+def test_client_cert(monkeypatch):
+    monkeypatch.setenv('JUJU_UNIT_NAME', 'test-tls')
+    client_cert()
+
+@pytest.fixture
+def server_cert():
+    from tlslib import server_cert as sc
+    sc("/tmp", 'tls-test.key')
+
+def test_server_cert(monkeypatch):
+    monkeypatch.setenv('JUJU_UNIT_NAME', 'test-tls')
+    server_cert()
