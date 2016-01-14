7654cf22e4a2dc893e202a7d9e2c614652550b0c
James Beedy <jamesbeedy@gmail.com>
2016-01-14 13:11:31 -0800

    Unit tests

--- unit_tests/test_actions.py
+++ unit_tests/test_actions.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import pytest
 import sys
+import os
 from pkg_resources import resource_filename
 
 # allow importing actions from the lib directory
@@ -15,21 +16,25 @@ def ca():
 def test_ca(monkeypatch):
     monkeypatch.setenv('JUJU_UNIT_NAME', 'test-tls')
     ca()
-
+    assert os.path.isfile('/tmp/tls-test.crt')
+ 
 @pytest.fixture
 def client_cert():
     from tlslib import client_cert as cc
-    cc("/tmp", 'tls-test.crt', 'tls-test.key')
+    cc("/tmp", 'tls-client-test.crt', 'tls-client-test.key')
 
 def test_client_cert(monkeypatch):
     monkeypatch.setenv('JUJU_UNIT_NAME', 'test-tls')
     client_cert()
+    assert os.path.isfile('/tmp/client.crt')
+    assert os.path.isfile('/tmp/client.key')
 
 @pytest.fixture
 def server_cert():
     from tlslib import server_cert as sc
-    sc("/tmp", 'tls-test.key')
+    sc("/tmp", '/tmp/client.key')
 
 def test_server_cert(monkeypatch):
     monkeypatch.setenv('JUJU_UNIT_NAME', 'test-tls')
     server_cert()
+    assert os.path.isfile('/tmp/client.key')
