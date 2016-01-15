e2f121fe50547363a402bc23714dd55e648fe20c
Matt Bruzek <matthew.bruzek@canonical.com>
2016-01-15 16:21:23 -0600

    Fixing the tests to run correctly from tox.

--- unit_tests/test_actions.py
+++ unit_tests/test_actions.py
@@ -2,21 +2,34 @@
 import pytest
 import sys
 import os
-from pkg_resources import resource_filename
 import tlslib
-# allow importing actions from the lib directory
-sys.path.append(resource_filename(__name__, '../lib'))
+
+from mock import patch
+
+def test_ca():
+    tlslib.ca('/tmp/tls', 'unit_tests/tls-test.crt')
+    assert os.path.isdir('/tmp/tls/')
+    assert os.path.isfile('/tmp/tls/ca.crt')
 
 
-@pytest.fixture
-def certificate_auth():
-    from tlslib import ca as cert_auth
-    cert_auth("/tmp", 'unit_tests/tls-test.crt')
+def test_client_cert():
+    tlslib.client_cert('/tmp/tls/client', 'unit_tests/tls-client-test.crt',
+                       'unit_tests/tls-client-test.key')
+    assert os.path.isdir('/tmp/tls/client')
+    assert os.path.isfile('/tmp/tls/client/client.crt')
+    assert os.path.isfile('/tmp/tls/client/client.key')
 
-def test_ca(certificate_auth):
 
-    tlslib.ca('/tmp', 'tls-test.crt')
-    assert os.path.isfile('/tmp/ca.crt')
+def test_server_cert():
+    with patch('tlslib.unitdata.kv') as kvpatch:
+        os.environ['JUJU_UNIT_NAME'] = 'tls-unit-name'
+        with open('unit_tests/tls-test.crt', 'r') as fp:
+            certificate_data = fp.read()
+        kvpatch.return_value.get.return_value = certificate_data
+        tlslib.server_cert('/tmp/tls/server', 'unit_tests/tls-test.key')
+        assert os.path.isdir('/tmp/tls/server')
+        assert os.path.isfile('/tmp/tls/server/server.key')
+        assert os.path.isfile('/tmp/tls/server/server.crt')
 #
 # @pytest.fixture
 # def client_cert():
