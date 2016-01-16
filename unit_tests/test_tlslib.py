5e384e7240412a90c2b6e805f652b259403c1a9b
Matt Bruzek <matthew.bruzek@canonical.com>
2016-01-15 18:05:59 -0600

    Tweaking the automated test control files to work better.

--- unit_tests/test_tlslib.py
+++ unit_tests/test_tlslib.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-import pytest
-import sys
 import os
 import tlslib
-
 from mock import patch
+
 
 def test_ca():
     tlslib.ca('/tmp/tls', 'unit_tests/tls-test.crt')
@@ -30,24 +28,3 @@ def test_server_cert():
         assert os.path.isdir('/tmp/tls/server')
         assert os.path.isfile('/tmp/tls/server/server.key')
         assert os.path.isfile('/tmp/tls/server/server.crt')
-#
-# @pytest.fixture
-# def client_cert():
-#     from tlslib import client_cert as cc
-#     cc("/tmp", 'tls-client-test.crt', 'tls-client-test.key')
-#
-# def test_client_cert(monkeypatch):
-#     monkeypatch.setenv('JUJU_UNIT_NAME', 'test-tls')
-#     client_cert()
-#     assert os.path.isfile('/tmp/client.crt')
-#     assert os.path.isfile('/tmp/client.key')
-#
-# @pytest.fixture
-# def server_cert():
-#     from tlslib import server_cert as sc
-#     sc("/tmp", '/tmp/client.key')
-#
-# def test_server_cert(monkeypatch):
-#     monkeypatch.setenv('JUJU_UNIT_NAME', 'test-tls')
-#     server_cert()
-#     assert os.path.isfile('/tmp/client.key')
